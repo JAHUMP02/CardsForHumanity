b45f52e85d95d50a512e5029b16bb032777dac48
JAHUMP02 <Joseph.humphrey2@stu.jefferson.kyschools.us>
2017-02-15 12:15:58 -0500

    Add files via upload
    New

--- MultiServer.java
+++ MultiServer.java
@@ -9,18 +9,24 @@ import java.util.Scanner;
 
 public class MultiServer extends Thread{
 	private ArrayList<Server> serverThreads=new ArrayList<Server>();
-	public void run(ServerSocket socket){
+	public Runnable run(ServerSocket socket){
+		while(true){
 		ForHumanity fh=new ForHumanity();
+		
 		Scanner in=new Scanner(System.in);
 		try{
 		boolean isInterrupted=false;
+		Socket ss=socket.accept();
+		int num=0;
+		ArrayList<String> names=new ArrayList<String>();
 		while(!isInterrupted){
-			Server thread=new Server(socket.accept());
+			Server thread=new Server(ss, num);
 			serverThreads.add(thread);
 			System.out.println(serverThreads.size());
 			if(in.nextBoolean()==true){
 				isInterrupted=true;
 			}
+			num++;
 		}
 		int[] score = new int[serverThreads.size()];
 		for(int i=0; i<score.length; i++){
@@ -47,18 +53,56 @@ public class MultiServer extends Thread{
 		}
 		President prez=new President(serverThreads.size());
 		
+		int[] choice=new int[serverThreads.size()];
+		
 		boolean winner=false;
 		while(winner==false){
-			int prezi=prez.nextczar(serverThreads.size());
+			
 			int[] inputs=new int[serverThreads.size()];
 			for(Server s:serverThreads){
-				s.OutputAll();
+				s.OutputAll("Welcome");
 			}
+			//for(Server s:serverThreads){
+			//	s.OutputAll("Enter name");
+			//	names.add(s.inputCatcher());
+			//}
+			for(String s:names){
+				System.out.println(s);
+			}
+			String blackCard=fh.dealBlack().getItems();
+			for(Server s:serverThreads){
+				s.OutputAll(blackCard);
+			}
+			for(int i=0; i<serverThreads.size(); i++){
+				//if(prez.nextczar()==i){
+					
+				//}else{
+				Server s=serverThreads.get(i);
+				for(int j=0; j<playercards[0].length; j++){
+					s.OutputAll(j+": "+playercards[i][j].getItems());
+				}
+				//}
+			}
+			for(int i=0; i<serverThreads.size(); i++){
+				//if(prez.nextczar()==i){
+				//}else{
+				Server s=serverThreads.get(i);
+				s.OutputAll("Input your choice");
+				System.out.println("l");
+				String string=s.inputCatcher();
+				System.out.println("HiLo");
+				choice[i]=Integer.getInteger
+						(string);
+				System.out.println(string);
+				//}
+			}
+			
 			System.out.println("Enter number of client");
-			serverThreads.get(in.nextInt()).OutputAll();
+			
 		}
 		}catch(Exception e){
 			e.printStackTrace();
+		}
 		}
 	}
 	public void doAll(int out, Socket ss) throws IOException{
