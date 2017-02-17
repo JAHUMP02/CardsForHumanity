2f4322dcca3402024d41408cfbfb15b9a7cbd0af
JAHUMP02 <Joseph.humphrey2@stu.jefferson.kyschools.us>
2017-02-17 12:24:22 -0500

    The Final Version For Eclipse

--- President.java
+++ President.java
@@ -1,14 +1,16 @@
 package com.example;
 
 public class President{
+	private int players;
 	public President(int p){
 		int czar = (int)(Math.random()*p);
 		currentczar = czar;
+		players=p;
 	}
 	private int currentczar = 0;
-	public int nextczar(int p){
+	public int nextczar(){
 		currentczar++;
-		if(currentczar > p-1){
+		if(currentczar > players-1){
 			currentczar = 0;
 		}
 		return currentczar;
